43f0e38b143027265f51f88e022e6a916ea39611
tfmcmahon <tfmcmahon@gmail.com>
2020-06-27 15:59:11 -0500

    leetcode 1334 retry

--- Graphs/Bellman-Ford/1334 Find the City With the Smallest Number of Neighbors at a Threshold Distance.js
+++ Graphs/Bellman-Ford/1334 Find the City With the Smallest Number of Neighbors at a Threshold Distance.js
@@ -1,47 +1,69 @@
 //https://leetcode.com/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/
 //bellman-ford
 
-const findTheCity = (n, edges, threshold) => {
+const findTheCity = (n, edges, d) => {
  
-  let reach = [...Array(n)].map(() => Array(n).fill(Infinity))
-  for (let i = 0; i < n; i++) {
-      reach[i][i] = 0
-  }
-  
-  const bellmanFord = reach => {
-      for (let i = 1; i < n; i++) {
-          let update = 0
-          for (let [u, v, w] of edges) {
-              if (reach[u] > reach[v] + w) {
-                  reach[u] = reach[v] + w
-                  update++
-              }
-              if (reach[v] > reach[u] + w) {
-                  reach[v] = reach[u] + w
-                  update++
-              }
+    //initialize a memo table to store all shortest path values
+    //we are going to find the shortest path to every node, giving each node a turn as the starting node
+    let memo = [...Array(n)].map(() => Array(n).fill(Infinity))
+    //create an adjacency list
+    let adjList = getMap(edges)
+    
+    //loop through the memo table
+    for (let i = 0; i < n; i++) {
+      
+      //initialize the starting node and queue
+      memo[i][i] = 0
+      let queue = [i]
+      
+      //perform BFS
+      while (queue.length) {
+        let u = queue.shift()
+        
+        if (adjList.get(u)) {
+          for (let [v, w] of adjList.get(u)) {
+            //if we found a shorter path to a node, update the memo table and add the node to the queue  
+            if (memo[i][v] > memo[i][u] + w) {
+              memo[i][v] =  memo[i][u] + w
+              queue.push(v)
+            }
           }
-          if (!update) break
+        }
       }
-  }
+    }
   
-  for (let i = 0; i < n; i++) bellmanFord(reach[i])
-  
-  const minCity = () => {
-      let min = Infinity
-      let city = -1
-      for (let i = 0; i < n; i++) {
-          let count = 0
-          for (let j = 0; j < n; j++) {
-              if (i !== j && reach[i][j] <= threshold) count++
-          }
-          if (min >= count) {
-              min = count
-              city = i
-          }
+    let min = Infinity
+    let city
+    
+    for (let i = 0; i < n; i++) {
+      let count = 0
+      for (let j = 0; j < n; j++) {
+        if (i !== j && memo[i][j] <= d) count++
       }
-      return city
-  }
+      if (min >= count) {
+        min = count
+        city = i
+      }
+    }
   
-  return minCity()
+    return city
+}
+  
+const getMap = edges => {
+
+    let adjList = new Map()
+
+    //iterate through the edge list
+    for (let [u, v, w] of edges) {
+        
+        //if the map doesn't have a node key, add it
+        if (!adjList.has(u)) adjList.set(u, [])
+        if (!adjList.has(v)) adjList.set(v, [])
+        
+        //add connected edge + weight pairs
+        adjList.get(u).push([v, w])
+        adjList.get(v).push([u, w])
+    }
+
+    return adjList
 }
