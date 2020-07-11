de9cae0dc2695ed8f4c08927e2ae6b11df5742aa
tfmcmahon <tfmcmahon@gmail.com>
2020-07-11 09:02:29 -0500

    leetcode 78 revisit

--- Other/78 Subsets.js
+++ Other/78 Subsets.js
@@ -2,14 +2,25 @@
 //back-tracking
 
 
+//strategy: create an result array with an empty array in it. Loop through the nums array,
+//performing something like level order traversal on the result array;
+//make a new copy of each result item and add nums[i] to it.
 const subsets = nums => {
-    
-  let result = [[]]
-  
-  //for each item in the nums array, create a copy of all previous subsets with the current number attached
+ 
+  //define the result array
+  let subsetsArr = [[]]
+
+  //loop through nums array
   for (let i = 0; i < nums.length; i++) {
-    result.forEach(sub => result.push([...sub, nums[i]]))
+
+    //set a level stopper to prevent infine loop
+    let j = subsetsArr.length
+    
+    //for every result item, push the current nums index value to it
+    while (j--) subsetsArr.push([...subsetsArr[j], nums[i]])
+
   }
   
-  return result
+  //total time = O(2 ^ n)
+  return subsetsArr
 }
