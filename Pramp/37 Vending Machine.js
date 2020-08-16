9070a91df8c6b00c6ea7edaef336b30a9d2ebd6c
tfmcmahon <tfmcmahon@gmail.com>
2020-08-16 17:29:43 -0500

    37 pramp

---
+++ Pramp/37 Vending Machine.js
@@ -0,0 +1,18 @@
+//OOP project
+
+//created a vending machine class which stocked beverages (another class)
+//it accepted money and dispensed change
+
+/*
+
+Name of peer: Tamas
+CS background: 
+
+What can I improve?: 
+What did my peer think I can improve?: Ask more clarifying questions earlier; anticipate more issues. We didn't anticipate how money is inserted into the machine.
+We also could have created a better change object and make change function that could accept different denominations down the road.
+
+What did I do well?: Successfully create two classes that interacted with eachother.
+What did peer think I did well?: Wrote usable code within the time limit
+
+*/
